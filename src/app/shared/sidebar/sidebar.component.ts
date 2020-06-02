31385e38d98db150d1e177b95903d275264f35d7
Israel Reyes Ozuna <iro1904@hotmailcom>
2020-06-02 14:18:02 -0500

    Redireccionamiento de Login a Dashboard y modificación de SideBar con services

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/services/service.index';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor (){}//(public sideBar: SidebarService) { }
+  constructor(public sideBar: SidebarService) { }
 
   ngOnInit(): void {
   }
